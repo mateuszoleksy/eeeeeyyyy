7722ada45140e6470d47395ca2edc692e2e09950
Mateusz Oleksy <45857054+mateuszoleksy@users.noreply.github.com>
2024-10-14 20:57:13 +0200

    update zadanie 10

--- zadanie10.cpp
+++ zadanie10.cpp
@@ -5,40 +5,41 @@
 using namespace std;
 int main()
 {
-    vector <string> numberOne;
-    vector <string> numberTwo;
+    vector <char> numberOne;
+    vector <char> numberTwo;
     string sentence { "" };
     int switchNumber = 0;
     cout << "Enter expression to solve: " << endl;
     cin >> sentence;
     for (int i = 0; i < sentence.size(); i++)
     {
-        if (sentence[i] != ' ')
+        if (sentence[i] != 32)
         {
-            if (sentence[i] == '+')
+            if (sentence[i] == 43)
             {
                 switchNumber = 1;
-            }
-            if (!switchNumber)
-            {
-                numberOne.push_back(sentence[i]);
-            }
-            else
-            {
-                numberTwo.push_back(sentence[i]);
+            }else {
+                if (!switchNumber)
+                {
+                    numberOne.push_back(sentence[i]);
+                }
+                else
+                {
+                    numberTwo.push_back(sentence[i]);
+                }
             }
         }
     }
-    int one;
-    int two;
-    for (int i = 0; i < numberOne.size(); i++)
+    int one {0};
+    int two {0};
+    for (int i = numberOne.size()-1; i >= 0; i--)
     {
-        one += numberOne[i] * pow(10, i);
+        one += (numberOne[i] - 48) * pow(10, (numberOne.size()-i-1));
     }
-     for (int i = 0; i < numberTwo.size(); i++)
+     for (int i = numberTwo.size()-1; i >= 0; i--)
         {
-         two += numberOne[i] * pow(10, i);
-        }
+         two += (numberTwo[i] - 48) * pow(10, (numberTwo.size()-i-1));
+         cout << numberTwo[i] << endl;        }
 
      cout << "The solution to expression is equal to: " << (one + two) << endl;
     return 0;
